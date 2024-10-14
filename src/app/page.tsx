93d898892363f63e65b062e996163813a854dcc6
Sarwar Hossain <sarwarhridoy4@gmail.com>
2024-10-14 13:53:01 +0600

    convert all page to typescript

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import EcgWaveHeart from "@/components/ECGHeart";
 import LoadingAnimation from "@/components/LoadingAnimation";
 import React from "react";
 
-const Home = () => {
+const Home: React.FC = () => {
   return (
     <>
       <EcgWaveHeart />
-      <LoadingAnimation/>
+      <LoadingAnimation />
     </>
   );
 };
